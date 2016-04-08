a1b81a64603d8081b17cf690d1876a6b104dccfa
Peter H. Froehlich <peter.hans.froehlich@gmail.com>
2016-04-07 20:22:01 -0400

    Removed some redundant types.

--- httpsify.go
+++ httpsify.go
@@ -20,12 +20,12 @@ import (
 const VERSION = "httpsify/v1"
 
 var (
-	port    *string = flag.String("port", "443", "the port that will serve the https requests")
-	cert    *string = flag.String("cert", "./cert.pem", "the cert.pem save-path")
-	key     *string = flag.String("key", "./key.pem", "the key.pem save-path")
-	domains *string = flag.String("domains", "", "a comma separated list of your site(s) domain(s)")
-	backend *string = flag.String("backend", "http://127.0.0.1:80", "the backend http server that will serve the terminated requests")
-	info    *string = flag.String("info", "yes", "whether to send information about httpsify or not ^_^")
+	port    = flag.String("port", "443", "the port that will serve the https requests")
+	cert    = flag.String("cert", "./cert.pem", "the cert.pem save-path")
+	key     = flag.String("key", "./key.pem", "the key.pem save-path")
+	domains = flag.String("domains", "", "a comma separated list of your site(s) domain(s)")
+	backend = flag.String("backend", "http://127.0.0.1:80", "the backend http server that will serve the terminated requests")
+	info    = flag.String("info", "yes", "whether to send information about httpsify or not ^_^")
 )
 
 // --------------
